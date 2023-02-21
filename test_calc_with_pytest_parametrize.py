47359e5262e4cbc2c4880e91082ef3b960a644b3
Yurii Demchenko <djdomovoy@gmail.com>
2023-02-21 20:27:34 +0200

    added TypeErrors tests

--- test_calc_with_pytest_parametrize.py
+++ test_calc_with_pytest_parametrize.py
@@ -9,3 +9,12 @@ from calc import calculator
 def test_calc(num1, num2, operator, expected_result):
     result = calculator(num1, num2, operator)
     assert result == expected_result
+
+
+@pytest.mark.parametrize(
+    "num1, num2, operator, expected_exception",
+    [(2, 0, "/", ZeroDivisionError), (2, "1", "/", TypeError)],
+)
+def test_calc_with_error(num1, num2, operator, expected_exception):
+    with pytest.raises(expected_exception):
+        calculator(num1, num2, operator)
